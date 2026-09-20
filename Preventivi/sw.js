3f7bd53a71b1e48d868f6be333db02177f992dd3
albertomazzocato-gif <alberto.mazzocato@gmail.com>
2026-09-20 15:59:41 +0200

    Add files via upload

---
+++ Preventivi/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'preventivi-cache-v2'; // <-- Modificato da v1 a v2
+const assetsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(assetsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
